17b128b99ebc1e8091197b76e51a1dc72d118a47
Neelesh Singh <ns9628491678@gmail.com>
2020-09-01 11:25:29 +0530

    first commit

---
+++ Profile_App/lib/main.dart
@@ -0,0 +1,102 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import './MyDrawer.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Profile App",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      drawer: MyDrawer(),
+      backgroundColor: Colors.deepOrange,
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/6.jpg'),
+            ),
+            Text(
+              'Neelesh Singh',
+              style: TextStyle(
+                fontFamily: 'All Fonts',
+                fontSize: 60.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.white,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.black,
+                ), 
+                onTap: (){},
+                title: Text(
+                  '+91 9628491678',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0,
+                      fontSize: 20.0),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.black,
+                ),
+                onTap: (){},
+                title: Text(
+                  'singhs.duos@gmail.com',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.0,
+                      fontSize: 20.0),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
